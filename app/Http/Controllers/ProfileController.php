57f1537131fcfcd81434c3855630a501ec836299
Shahjahan Ahamed Saju <sasaju1283@gmail.com>
2024-06-24 22:57:58 +0600

    Modify variable name
    Modify variable name

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,15 +18,15 @@ class ProfileController extends Controller
             'age' => $age
         ];
         
-        $cookieName = 'access_token';
-        $cookieValue = '123-XYZ';
-        $cookieMinutes = 1;
-        $cookiePath = '/';
-        $cookieDomain = $_SERVER['SERVER_NAME'];
-        $cookieSecure = false;
-        $cookieHttpOnly = true;
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
 
-        $cookie = Cookie::make($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
+        $cookie = Cookie::make($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
 
         return response()->json($data, 200)->cookie($cookie);
     }
