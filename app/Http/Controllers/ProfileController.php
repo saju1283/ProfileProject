73c43614e30484d11a689ac717db51cc7ca1b99b
Shahjahan Ahamed Saju <sasaju1283@gmail.com>
2024-12-14 21:59:48 +0600

    Update Code
    Update Code

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,23 +11,23 @@ class ProfileController extends Controller
     {
         $name = "Donald Trump";
         $age = "75";
-    
+   
         $data = [
             'id' => $id,
             'name' => $name,
             'age' => $age
         ];
    
-        $cookieName = 'access_token';
-        $cookieValue = '123-XYZ';
-        $cookieMinutes = 1;
-        $cookiePath = '/';
-        $cookieDomain = $_SERVER['SERVER_NAME'];
-        $cookieSecure = false;
-        $cookieHttpOnly = true;
- 
-        $cookie = Cookie::make($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
-  
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+   
+        $cookie = Cookie::make($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
         return response()->json($data, 200)->cookie($cookie);
     }
 }
